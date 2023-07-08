025ccb9068be40caad44f7260a74735428408b8d
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-07-08 23:50:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-const library = [
-  {
-    author: 'Author 1',
-    title: 'Book 1',
-    readingStatus: true
-  },
-  {
-    author: 'Author 2',
-    title: 'Book 2',
-    readingStatus: false
-  },
-  {
-    author: 'Author 3',
-    title: 'Book 3',
-    readingStatus: true
+function numberOfBooksRead(library) {
+  let count = 0;
+  for (let i = 0; i < library.length; i++) {
+    if (library[i].readingStatus === true) {
+      count++;
+    }
   }
-];
-
-const numBooksRead = numberOfBooksRead(library);
-console.log(numBooksRead); // Output: 2
+  return count;
+}
